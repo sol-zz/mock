8b274baa69f189598e88bec7b8ebbb973407fc3f
sol-zz <jinsol9770@naver.com>
2024-10-21 13:44:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,6 @@ import streamlit as st
 import json
 import requests
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 def get_domain(query, chat_history=None):
     url = 'https://clovastudio.stream.ntruss.com/testapp/v1/chat-completions/HCX-DASH-001'
     headers = {
